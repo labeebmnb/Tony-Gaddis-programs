f9940f18233b102b67a1e48e8e8cc3de986dc7dd
Labeeb <97628411+labeebmnb@users.noreply.github.com>
2022-01-18 11:35:01 +0500

    Add files via upload

---
+++ chapter 2.cpp
@@ -0,0 +1,234 @@
+#include<iostream>
+#include<math.h>
+#include<math.h>
+#include<cstdlib>
+using namespace std;
+int main()
+
+
+
+
+//sum of two numbers
+//{
+//	int fifty = 50, hundred = 100, total = 0;
+//	total = fifty + hundred;
+//	cout << "fifty=" << fifty << endl << "hundred=" << hundred << endl;
+//	cout << "total="<<total;
+//	return 0;
+//}
+
+
+
+
+//sales predicion
+//{
+//	float percent = 0.58, sales = 8.6e6;
+//	sales = percent *sales;
+//	cout << "company will generate $" << sales << " million this year." << endl;
+//	return 0;
+//}
+
+
+
+
+//sales tax
+//{
+//	int purchase = 95;
+//	float state_sales_tax = 0.04, country_sales_tax = 0.02, total_tax = 0;
+//	total_tax = (purchase*state_sales_tax) + (purchase*country_sales_tax);
+//	cout << "total state and country tax on $95 purchase will be " << total_tax<<"%";
+//	return 0;
+//}
+
+
+
+//Restaurant bill
+//{
+//	float meal_charge = 88.67, 
+//		tax = 0.0675, 
+//		tip = 0.2,
+//		meal_total_cost = 0,
+//		meal_tax = 0,
+//		meal_tip = 0;
+//	meal_tax = meal_charge * tax;
+//	meal_tip = (meal_charge+meal_tax) * tip;
+//	meal_total_cost = meal_charge + meal_tax + meal_tip;
+//	cout << "meal cost is $" << meal_charge << endl << "meal tax is $" << meal_tax << endl << "meal tip is $" << meal_tip << endl;
+//	cout << "meal total cost is $" << meal_total_cost << endl;
+//	return 0;
+//}
+
+
+
+
+//avg of values
+//{
+//	int a = 28, b = 32, c = 37, d = 24, e = 33, sum = 0, avg = 0;
+//	sum = a + b + c + d + e;
+//	avg = sum / 5;
+//	cout << "average is " << avg;
+//	return 0;
+//}
+
+
+
+
+//annual pay
+//{
+//	int payAmount = 2200,
+//		payPeriods = 26,
+//		annualPay = 0;
+//	annualPay = payAmount * payPeriods;
+//	cout << "annual pay is $" << annualPay;
+//	return 0;
+//}
+
+
+
+
+//oceon level
+//{
+//	const float MM = 1.5;
+//		float five_years = 5, seven_years = 7, ten_years = 10, five = 0, seven = 0, ten = 0;
+//	five = MM * five_years;
+//	seven = MM * seven_years;
+//	ten = MM * ten_years;
+//	cout << "ocean level after 5 years will be " << five << endl;
+//	cout << "ocean level after 7 years will be " << seven << endl;
+//	cout << "ocean level after 10 years will be " << ten << endl;
+//	return 0;
+//}
+
+
+
+//total purchase
+//{
+//float tax = 0.07,
+////a,b,c,d,e are items.
+//a = 15.95,
+//b = 24.95,
+//c = 6.95,
+//d = 12.95,
+//e = 3.96,
+//subtotal = 0,
+//sales_tax = 0,
+//total = 0;
+//subtotal = a + b + c + d + e;
+//sales_tax = subtotal * tax;
+//total = subtotal + sales_tax;
+//cout << "price of item 1 is $ " << a << endl;
+//cout << "price of item 2 is $ " << b << endl;
+//cout << "price of item 3 is $ " << c << endl;
+//cout << "price of item 4 is $ " << d << endl;
+//cout << "price of item 5 is $ " << e << endl;
+//cout << "subtotal is $ " << subtotal << endl;
+//cout << "amount of sales tax is $" << sales_tax<<endl;
+//cout << "total is $" << total << endl;
+//return 0;
+//}
+
+
+
+//cyborg data type sizes
+//{
+//cout << "size of char is " << sizeof(char) << " byte" << endl;
+//cout << "size of int is " << sizeof(int) << " bytes" << endl;
+//cout << "size of float is " << sizeof(float) << " bytes" << endl;
+//cout << "size of double is " << sizeof(double) << " bytes" << endl;
+//}
+
+
+
+
+//miles per gallon
+//{
+//int gallons = 15,
+//distance = 375,
+//MPG = 0; //MPG is miles per gallon.
+//MPG = distance / gallons;
+//cout << "MPG is " << MPG;
+//
+//}
+
+
+
+//distance per tank of gas
+//{
+//int gallons = 20;
+//float townMPG = 23.5,
+//highwayMPG = 28.9,
+//townDistance = 0,
+//highwayDistance = 0;
+//townDistance = gallons * townMPG;
+//highwayDistance = gallons * highwayMPG;
+//cout << "car can travel " << townDistance << " miles in town on one tank." << endl;
+//cout << "car can travel " << highwayDistance << " miles on highway on one tank." << endl;
+//}
+
+
+
+
+//land calculation
+//{
+//int one_acre = 43560, land = 391876, acres = 0;
+//acres = land / one_acre;
+//cout << "number of acres in a track of land 391,876 square feet is " << acres;
+//}
+
+
+
+
+//circuit board price
+//{
+//float profit = 0.35, cost = 14.95, selling_cost = 0;
+//selling_cost = (profit * cost) + cost;
+//cout << "selling price of board is $" << selling_cost;
+//}
+
+
+
+//personal information
+//{
+//cout << "Labeeb" << endl << "house no.a,lahore,pakistan,0000" << endl << "03014666999" << endl << "computer science" << endl;
+//}
+
+
+
+//triangle pattern 
+// do yourself
+
+
+
+
+//diamond pattern
+//do yourself
+
+
+
+//stock commission
+//{
+//int shares = 750, price = 35, amount_paid = 0;
+//float commission = 0.02, amount_of_commission = 0, total_amount=0;
+//amount_paid = shares * price;
+//amount_of_commission = commission * amount_paid;
+//total_amount = amount_of_commission + amount_paid;
+//cout << "amount paid for stock alone is $" << amount_paid << endl;
+//cout << "ammount of commision is $" << amount_of_commission << endl;
+//cout << "total amount paid is $" << total_amount;
+//}
+
+
+
+
+//energy drink consumption
+//{
+//int customers = 16500;
+//float one_or_more = 0.15,
+//citrus_percent = 0.58,
+//customers_of_one_or_more = 0,
+//customers_prefer_citrus = 0;
+//customers_of_one_or_more = customers * one_or_more;
+//customers_prefer_citrus = customers_of_one_or_more * citrus_percent;
+//cout << "number of customers in survey who purchase one or more energy drinks per week are " << customers_of_one_or_more << endl;
+//cout << "number of customers prefer citrus-flavoured energy drinks are "<<customers_prefer_citrus;
+//}
